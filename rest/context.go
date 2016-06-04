41661c092efb45c7e824223e76c9ec6e9080119d
mkorman9 <dynax126@gmail.com>
2016-06-04 19:40:58 +0200

    Recovery handler

--- rest/context.go
+++ rest/context.go
@@ -23,9 +23,9 @@ func RestAppContext(routes []Route, config RestConfig) *mux.Router {
 func initRouter(routes []Route) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
-		var handler http.Handler
-		handler = route.HandlerFunc
+		var handler http.Handler = route.HandlerFunc
 		handler = AccessLogsHandler(handler, route.Name)
+		handler = RecoverHandler(handler)
 
 		router.
 		Methods(route.Method).
---
+++ rest/recovery_handler.go
@@ -0,0 +1,38 @@
+package rest
+
+import (
+	"net/http"
+	"errors"
+
+	"github.com/op/go-logging"
+)
+
+func RecoverHandler(h http.Handler) http.Handler {
+	var log = logging.MustGetLogger("panic_log")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		var err error
+		defer func() {
+			r := recover()
+			if r != nil {
+				switch t := r.(type) {
+				case string:
+					err = errors.New(t)
+				case error:
+					err = t
+				default:
+					err = errors.New("Unknown error")
+				}
+				log.Errorf(
+					"%s\t%s\t%s\tFatal error during request handling: %s",
+					req.RemoteAddr,
+					req.Method,
+					req.RequestURI,
+					err.Error(),
+				)
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, req)
+	})
+}
