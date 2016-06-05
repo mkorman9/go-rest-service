e077f9abfc68d09ef3d5657bd7dae941fd4e7300
mkorman9 <dynax126@gmail.com>
2016-06-05 21:18:43 +0200

    Saving cats

--- cat.go
+++ cat.go
@@ -1,7 +1,7 @@
 package main
 
 type Cat struct {
-	Id				int64	`json:"id" db:"ID"`
+	Id				int64	`json:"id,omitempty" db:"ID"`
 	RoleName 		string	`json:"roleName" db:"ROLE_NAME"`
 	Name 			string 	`json:"name" db:"NAME"`
 	DuelsWon 		int		`json:"duelsWon" db:"DUELS_WON"`
--- handlers.go
+++ handlers.go
@@ -21,4 +21,11 @@ func Save(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
+	db := rest.GetContext().GetMember("db_Default").(*sqlx.DB)
+	tx := db.MustBegin()
+	tx.NamedExec("INSERT INTO CAT (ROLE_NAME, NAME, DUELS_WON) VALUES (:ROLE_NAME, :NAME, :DUELS_WON)", &cat)
+	tx.Commit()
+
+	rest.RespondJson(w, http.StatusOK, "ok")
 }
